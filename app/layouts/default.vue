369781457ff8acf2fb5b9cc00d5dff26b1259c33
K1ender <146767945+K1ender@users.noreply.github.com>
2025-06-13 21:14:54 +0300

    feat: some seo fixes

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,5 +1,10 @@
 <script lang="ts" setup>
-
+useSeoMeta({
+  title: "Receiptify",
+  description: "Turn your receipts into a Cashew link",
+  ogTitle: "Receiptify",
+  ogDescription: "Turn your receipts into a Cashew link",
+})
 </script>
 
 <template>
